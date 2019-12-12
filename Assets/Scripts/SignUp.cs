1ea29daba567dd2b6668e92721e9174c529e4605
perfectdark420 <47585882+perfectdark420@users.noreply.github.com>
2019-12-12 16:25:44 +1100

    LoginWorks

---
+++ Assets/Scripts/SignUp.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+
+
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+public class SignUp : MonoBehaviour
+{
+
+    #region Variables 
+    [Header("Sign Up")]
+    public TMP_InputField SignUp_userNameText;
+    public TMP_InputField SignUp_userEmailText;
+    public TMP_InputField SignUp_userPasswordText;
+    public TMP_InputField SignUp_userConfirmPasswordText;
+
+    string createUserURL = "http://localhost/squealsystem/InsertUser.php";
+
+    [Header("Log In")]
+    public TMP_InputField LogIn_userNameText;
+    public TMP_InputField LogIn_userPasswordText;
+
+    string loginUserURL = "http://localhost/squealsystem/LoginUser.php";
+
+    string inputUserName;
+    string inputEmail;
+    string inputPassword;
+
+
+    #endregion
+
+    #region SignUp
+    public void SubmitSignUpInfo()
+    {
+        if (SignUp_userPasswordText.text == SignUp_userConfirmPasswordText.text)
+        {
+            inputUserName = SignUp_userNameText.text;
+            inputEmail = SignUp_userEmailText.text;
+            inputPassword = SignUp_userPasswordText.text;
+
+            StartCoroutine(CreateUser(inputUserName, inputEmail, inputPassword));
+        }
+        else
+        {
+            print("Passwords do not match");
+        }
+        
+    }
+
+    IEnumerator CreateUser(string username, string email, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("emailPost", email);
+        form.AddField("passwordPost", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL, form);
+        yield return webRequest.SendWebRequest();
+        
+
+        string returnInfoString = webRequest.downloadHandler.text;
+        print(returnInfoString);
+
+    }
+    #endregion
+    #region Login 
+    public void SubmitLoginInfo()
+    {
+        inputUserName = LogIn_userNameText.text;
+        inputPassword = LogIn_userPasswordText.text;
+
+        StartCoroutine(LogInUser(inputUserName, inputPassword));
+    }
+
+    IEnumerator LogInUser(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(loginUserURL, form);
+        yield return webRequest.SendWebRequest();
+
+        string returnInfoString = webRequest.downloadHandler.text;
+        print(returnInfoString);
+
+        if(returnInfoString == "Login Success")
+        {
+            print("Login Success");
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SQueaLsystem/LoginUser.php
@@ -0,0 +1,45 @@
+<?php
+	$server_name = "localhost";
+	$server_user = "root";
+	$server_password = "";
+	$database_name = "SQueaLsystem";
+	
+	$username = $_POST["usernamePost"];
+	$password = $_POST["passwordPost"];
+
+	//establishes connection to database
+	$conn = new mysqli($server_name, $server_user, $server_password, $database_name);
+		//if it fails to gain a connection to the database
+	if(!$conn)
+	{
+		//kill connection, return connection failed
+		die("Connection Failed.". mysqli_connect_error());
+	}
+	//Locates and choses password from users table where username data from Login script matches database user
+	$finduser = "SELECT password FROM users WHERE username = '".$username."' ";
+	$finduserresult = mysqli_query($conn, $finduser);//finds user result, connects
+
+	
+	//Get the result and confirm login
+	if(mysqli_num_rows($finduserresult)>0)
+	{
+		//search through the rows
+		while($row = mysqli_fetch_assoc($finduserresult))
+		{
+			//if it finds a matching password from a selected username in the password row
+			if($row['password'] == $password)
+			{
+				echo "Login success";
+				//Login success
+			}
+			else
+			{
+				//wrong password dummy
+				echo "password incorect";	
+			}
+		}
+	} else {
+		echo "user not found";
+		//couldnt find a matching username
+	}
+?>
